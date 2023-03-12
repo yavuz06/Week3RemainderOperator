e8f1df239fc085dde989da25f6ef05ee8612a478
yavuz06 <yavuzselmkaya@gmail.com>
2023-03-12 19:06:11 +0300

    week3part1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//odd or even
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("Enter your number");
+int a = Int32.Parse(Console.ReadLine());
+
+int result = a % 2;
+Console.WriteLine(result);
+
+if (result == 0)
+{
+    Console.WriteLine("This is an even number");
+}
+else
+{
+    Console.WriteLine("This is an odd number");
+}
+
+//switch (result)
+//{
+//    case 0: Console.WriteLine("This is an even number");
+//            break;
+//        case 1: Console.WriteLine("This is an odd number");
+//        break;
+      
+//}
